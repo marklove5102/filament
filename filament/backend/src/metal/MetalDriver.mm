fed4f0523f6a57e4407591955ba830e2e279b037
Mathias Agopian <mathias@google.com>
2025-10-27 12:28:58 -0700

    FrameInfo now contains compositor timings
    - the presentation deadline
    - the refresh rate from the display
    - the composition-display latency

--- filament/backend/src/metal/MetalDriver.mm
+++ filament/backend/src/metal/MetalDriver.mm
@@ -1479,8 +1479,13 @@ bool MetalDriver::isCompositorTimingSupported() {
     return false;
 }
 
+bool MetalDriver::queryCompositorTiming(backend::SwapChainHandle swapChain,
+        CompositorTiming* outCompositorTiming) {
+    return false;
+}
+
 bool MetalDriver::queryFrameTimestamps(SwapChainHandle swapChain, uint64_t frameId,
-        backend::FrameTimestamps* outFrameTimestamps) {
+        FrameTimestamps* outFrameTimestamps) {
     return false;
 }
 
